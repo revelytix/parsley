de0576e37efe5ff817c9c0098f729b9dd27663ea
puredanger <alexdmiller@yahoo.com>
2012-08-09 15:10:13 -0500

    aot compile from the root

--- project.clj
+++ project.clj
@@ -2,5 +2,8 @@
   :description "a generator of total and truly incremental parsers"
   :url "http://github.com/cgrand/parsley/"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.cgrand/regex "1.1.0"]])
+                 [net.cgrand/regex "1.1.0"]]
+
+  :aot [net.cgrand.parsley]
+)
 
